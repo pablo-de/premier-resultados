7f389994a59c0e42e527133a3ac7cc005710cd62
Pablo DE <diazechevestepablo@gmail.com>
2022-01-07 11:24:28 -0300

    Agregando datos de API, mejoras varias

--- main.py
+++ main.py
@@ -1,20 +1,63 @@
 from flask import Flask, json
-from flask import render_template
+from flask import render_template, jsonify
 import requests
+import datetime
 
 app = Flask(__name__)
 
+SECRET_KEY = ""
+id_competitions = '2021'
+api = 'https://api.football-data.org'
 
-@app.route('/')
+fecha = datetime.datetime.utcnow()
+
+def competition():
+    competitionsUrl = '/v2/competitions/' + id_competitions
+    headers = { 'X-Auth-Token': SECRET_KEY }
+    response = requests.get(api + competitionsUrl, headers = headers)
+    response.raise_for_status()
+    content = response.json()
+    currentSeason = content['currentSeason']
+    matchday = currentSeason['currentMatchday']
+    return str(matchday)
+
+#Traer desde la API los datos para armar la tabla de posiciones
 def standings():
-    SECRET_KEY = ""
-    id_competitions = '2021'
-    api = 'https://api.football-data.org'
-    uri = '/v2/competitions/' + id_competitions + '/standings'
+    urlStandings = '/v2/competitions/' + id_competitions + '/standings'
     headers = { 'X-Auth-Token': SECRET_KEY }
 
-    response = requests.get(api + uri, headers = headers)
+    response = requests.get(api + urlStandings, headers = headers)
     response.raise_for_status()
     content = response.json()
     table = content['standings'][0]['table']
-    return render_template("index.html", data = table)
+    return table
+
+#Traer desde la API los datos para los resultados de los partidos
+def matches():
+    matchday = competition()
+    urlMatches = '/v2/competitions/' + id_competitions + '/matches?matchday=' + matchday
+    headers = { 'X-Auth-Token': SECRET_KEY }
+    response = requests.get(api + urlMatches, headers = headers)
+    response.raise_for_status()
+    content = response.json()
+    fixture = content['matches']
+    return fixture
+
+#Testear json
+@app.route('/data_json')
+def data_json():
+    dummy_data = matches()
+    return jsonify(dummy_data)
+
+#Routeo a paginas.
+@app.route('/')
+@app.route('/home')
+def home_page():
+    partidos = matches()
+    return render_template("index.html", data = partidos)
+
+@app.route('/posiciones')
+def tabla_page():
+    posiciones = standings()
+    return render_template("tabla.html", data = posiciones)
+
