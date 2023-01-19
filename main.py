0e4277ce5e11508b2191ccf944708ee92f009e82
Pablo <pablo.diaz@cloudshinetech.com>
2023-01-19 14:42:00 -0300

    fix project structure

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import render_template, jsonify
 import requests
 import datetime
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -12,60 +14,65 @@ api = 'https://api.football-data.org'
 
 fecha = datetime.datetime.utcnow()
 
+# Filtro para modificar el formato de las fechas
+@app.template_filter('formatdatetime')
+def format_datetime(value):
+    cr_date = value
+    new_date = datetime.datetime.strptime(cr_date[:10], '%Y-%m-%d')
+    return new_date.strftime("%d-%m-%Y")
+
 def competition():
     competitionsUrl = '/v2/competitions/' + id_competitions
-    headers = { 'X-Auth-Token': SECRET_KEY }
-    response = requests.get(api + competitionsUrl, headers = headers)
+    headers = {'X-Auth-Token': SECRET_KEY}
+    response = requests.get(api + competitionsUrl, headers=headers)
     response.raise_for_status()
     content = response.json()
     currentSeason = content['currentSeason']
     matchday = currentSeason['currentMatchday']
     return str(matchday)
 
-#Traer desde la API los datos para armar la tabla de posiciones
+# Traer desde la API los datos para armar la tabla de posiciones
 def standings():
     urlStandings = '/v2/competitions/' + id_competitions + '/standings'
-    headers = { 'X-Auth-Token': SECRET_KEY }
-
-    response = requests.get(api + urlStandings, headers = headers)
+    headers = {'X-Auth-Token': SECRET_KEY}
+    response = requests.get(api + urlStandings, headers=headers)
     response.raise_for_status()
     content = response.json()
     table = content['standings'][0]['table']
     return table
 
-#Traer desde la API los datos para los resultados de los partidos
+# Traer desde la API los datos para los resultados de los partidos
 def matches():
     matchday = competition()
-    urlMatches = '/v2/competitions/' + id_competitions + '/matches?matchday=' + matchday
-    headers = { 'X-Auth-Token': SECRET_KEY }
-    response = requests.get(api + urlMatches, headers = headers)
+    urlMatches = '/v2/competitions/' + \
+        id_competitions + '/matches?matchday=' + matchday
+    headers = {'X-Auth-Token': SECRET_KEY}
+    response = requests.get(api + urlMatches, headers=headers)
     response.raise_for_status()
     content = response.json()
     fixture = content['matches']
     return fixture
 
 
-#Testear json
+############# Routes #######################
+
+# Testear json
 @app.route('/data_json')
 def data_json():
     dummy_data = standings()
     return jsonify(dummy_data)
 
-#Routeo a paginas.
 @app.route('/')
 @app.route('/home')
 def home_page():
     partidos = matches()
-    return render_template("index.html", data = partidos)
+    return render_template("index.html", data=partidos)
 
 @app.route('/posiciones')
 def tabla_page():
     posiciones = standings()
-    return render_template("tabla.html", data = posiciones)
+    return render_template("tabla.html", data=posiciones)
 
-#Filtro para modificar el formato de las fechas
-@app.template_filter('formatdatetime')
-def format_datetime(value):
-    cr_date = value
-    new_date = datetime.datetime.strptime(cr_date[:10], '%Y-%m-%d')
-    return new_date.strftime("%d-%m-%Y")
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error.html"), 404
